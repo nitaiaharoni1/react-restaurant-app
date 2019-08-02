b03fc60f6be14590929fe9715b97d806cc556c99
Nitai <nitaiaharoni1@gmail.com>
2019-08-02 21:30:47 +0300

    nodejs

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express'),
+    path = require('path'),
     bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.Port || 3000;
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/hello', (req, res) => {
-    res.send({express : 'Hello World!'})
+    res.send({express: 'Hello World!'})
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
