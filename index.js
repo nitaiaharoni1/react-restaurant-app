a7ea72c98de7ed32a4bda9f694f2508286203412
Nitai <nitaiaharoni1@gmail.com>
2019-08-02 21:47:19 +0300

    nodejs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express'),
     bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(bodyParser.json());
@@ -13,8 +13,8 @@ app.get('/hello', (req, res) => {
     res.send({express: 'Hello World!'})
 });
 
-app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendfile(path.join(__dirname = 'client/build/index.html'));
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
