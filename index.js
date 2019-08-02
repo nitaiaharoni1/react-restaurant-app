693bb8375fe1c40de010682033a0cbb240c770ef
Nitai <nitaiaharoni1@gmail.com>
2019-08-02 21:27:18 +0300

    nodejs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express'),
 
 const app = express();
 const port = process.env.Port || 3000;
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
