429b524c6fcdd89fed5832ab3c64640c2dbd9ca4
I355539 <nitai.aharoni@sap.com>
2019-08-05 19:11:21 +0300

    version

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//app.get('*', (req, res) => {
-//    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-//});
+app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+});
 
 app.get('/api/gallery', async (req, res) => {
     let images = await fs.readdir('./server_assets');
