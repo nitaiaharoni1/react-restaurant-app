bd53a18006f1dbc987b749b4749a5deee661be87
I355539 <nitai.aharoni@sap.com>
2019-08-05 18:28:07 +0300

    version

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendfile(path.join(__dirname = 'client/build/index.html'));
 });
 
