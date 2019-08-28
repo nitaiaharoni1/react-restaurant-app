376cab6c5dfaacce60516fa293a54b4477df7e10
I355539 <nitai.aharoni@sap.com>
2019-08-28 16:19:48 +0300

    version

--- README.md
+++ README.md
@@ -1,18 +1,64 @@
-## Mama's Restaurant - React Ecommerce website
+# Mama's Restaurant - React ecommerce website
 Mama's Restaurant is an ecommerce website built using React, Redux integrated with Node.js and Express backend.
 
 A demo for the Website App is available [here](https://mamas-restaurant-app.herokuapp.com/).
 
-#### Screenshots
 ![mamas restaurant](website.gif "mamas restaurant")
 
+## Project Questions
 
-#### Prerequisites
+#### What are you selling?
+* Our store is basically a restaurant and we sell food through delivery to the customer's house.
+#### What additional page did you add? How to operate them?
+* Home page `/home`:
+    * 
+* Deliveries page `/home`:
+    * 
+* Gallery page `/gallery`:
+    * 
+* Menu pages `/menu`:
+    * 
+* Admin page `/admin`:
+    * 
+* Login page `/login`:
+    * 
+* Signup page `/signup`:
+    * 
+* Cart page `/cart`:
+    * 
+* Checkout page `/chekout`:
+    * 
+* Terms of service page `/terms`:
+    * 
+* Page not found page `/any other`:
+    * 
+
+#### What was hard to do?
+* Learn React
+* Learn Bootstrap 
+* Learn Redux framework
+* Learn Jest testing framework
+* Integrate with Node Express and save the data
+
+#### Who is your partner? name and id?
+* Me: `Nitai Aharoni 203626742`
+* Partner: `Nadav Lotan 312346406`
+
+#### Specify all the different route your app supports
+#### How did you make your store secured?
+* Validating all forms
+* Validating admin user for Admin page
+* Validating requests on the server
+
+#### Did you implement the store using react.js? 
+* Yes, React and Redux
+
+## Prerequisites
 1. Git
 2. Node v10.16.1
 3. A fork/clone of this repo
 
-#### Running locally
+## Running locally
 1. In react-restaurant-website directory run in terminal: `npm run app`
 2. Open http://localhost:3000
 
--- client/src/pages/Readme.js
+++ client/src/pages/Readme.js
@@ -16,21 +16,47 @@ class Readme extends Component {
                 <CustomParallax title='Readme' text="Nitai Aharoni's Readme" img={home_top} height='20em'/>
                 <Container>
                     <article className="markdown-body entry-content p-5" itemProp="text">
+                        <hr/>
                         <h2>Mama's Restaurant - React ecommerce website</h2>
-                        <p>Mama's Restaurant is an ecommerce website built using React, Redux integrated with Node.js and Express backend.</p>
-                        <p>A demo for the Website App is available <a href="https://mamas-restaurant-app.herokuapp.com/" rel="nofollow">here</a>.</p>
-                        <h4>Screenshots</h4>
-                        <p><a target="_blank" rel="noopener noreferrer"
-                              href="/nitaiaharoni1/react-restaurant-website/blob/master/images/mobile-apps.png"><img
-                            src="/nitaiaharoni1/react-restaurant-website/raw/master/images/mobile-apps.png" alt="Mobile Apps" title="Mobile Apps"
-                            style={{maxWidth: '100%'}}/></a></p>
-                        <h4>Prerequisites</h4>
+                        <p>Mama's Restaurant is an ecommerce website built using React, Redux integrated with Node.js and Express backend.
+                            <br/>
+                            The deployed Website is available <a href="https://mamas-restaurant-app.herokuapp.com/" rel="nofollow">here</a>.
+                        </p>
+
+                        <hr/>
+                        <h3>Project Questions</h3>
+
+                        <h5>What are you selling?</h5>
+                        <p>Our store is basically a restaurant and we sell food through delivery to the customer's house</p>
+
+                        <h5>What additional page(s) did you add? How to operate them?</h5>
+                        <p></p>
+
+                        <h5>What was hard to do?</h5>
+                        <p></p>
+
+                        <h5>Who is your partner? name and id. What did you do? What did your partner do?</h5>
+                        <p></p>
+
+                        <h5>Specify all the different route your app supports</h5>
+                        <p></p>
+
+                        <h5>How did you make your store secured?</h5>
+                        <p></p>
+
+                        <h5>Did you implement the store using react.js? </h5>
+                        <p></p>
+                        <hr/>
+
+                        <h3>Prerequisites</h3>
                         <ol>
                             <li>Git</li>
-                            <li>Node v10.16.1</li>
+                            <li>Node v10.15.1</li>
                             <li>A fork/clone of this repo</li>
                         </ol>
-                        <h4>Running locally</h4>
+                        <hr/>
+
+                        <h3>Running locally</h3>
                         <ol>
                             <li>In react-restaurant-website directory run in terminal: <code>npm run app</code></li>
                             <li>Open <a href="http://localhost:3000" rel="nofollow">http://localhost:3000</a></li>
