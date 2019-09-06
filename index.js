2913e505faf5608b7081d60aefdafe97ef35840b
I355539 <nitai.aharoni@sap.com>
2019-09-06 16:19:39 +0300

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const SECRET = 'secret',
     port = process.env.PORT || 3005,
     limiter = rateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // limit each IP to 100 requests per windowMs
+        max: 1000, // limit each IP to 100 requests per windowMs
         message: "Too many requests from this IP, please try again later"
     });
 
